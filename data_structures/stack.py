29ef87233b5863fa954c2eea88ee36f9738cd2f3
geetheswar-v <142201025@smail.iitpkd.ac.in>
2023-09-08 16:53:09 +0530

    Added ListStack

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -20,3 +20,28 @@ class Stack:
 
     def isempty(self):
         return self.__stack.isempty()
+
+
+class ListStack:
+
+    def __init__(self):
+        self.__stack = []
+
+    def push(self, value):
+        self.__stack.append(value)
+
+    def pop(self):
+        if self.isempty():
+            return None
+        return self.pop()
+
+    def top(self):
+        if self.isempty():
+            return None
+        return self.__stack[-1]
+
+    def isempty(self):
+        return len(self.__stack) == 0
+
+    def len(self):
+        return len(self.__stack)
